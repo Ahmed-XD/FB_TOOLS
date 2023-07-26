d0277018ccea17711e12d7c339daac895f00cccd
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2023-07-26 15:57:47 +0600

    Update FB_TOOLS.py

--- FB_TOOLS.py
+++ FB_TOOLS.py
@@ -7,7 +7,7 @@ print('Join Our Facebook Group and support us')
 os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
 os.system('clear')
 print('-'*54)
-users_choice = input("FB_TOOLS MADE BY AHMED ALI\n[1] CHECK CLONED ACCOUNTS LIVE OR DEAD\n[2] CHECk CLONED ACCOUNTS TOTAL FRIEND\n\nChoose : ")
+users_choice = input("\033[1;33mFB_TOOLS MADE BY AHMED ALI\n[1] CHECK CLONED ACCOUNTS LIVE OR DEAD\n[2] CHECk CLONED ACCOUNTS TOTAL FRIEND\n\nChoose : ")
 if users_choice == "1":
   os.system("clear")
   os.system("chmod +x ID_CHK")
